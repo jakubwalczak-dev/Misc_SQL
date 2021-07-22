30c3f9d11733fdd58230cc6455769fe4aea1c355
jakubwalczak-dev <84325306+jakubwalczak-dev@users.noreply.github.com>
2021-07-22 17:48:08 +0200

    Update RSPC_Hierarchy.sql

--- RSPC_Hierarchy.sql
+++ RSPC_Hierarchy.sql
@@ -1,5 +1,4 @@
---CREATE OR REPLACE VIEW "RSPC_HIERARCHY" AS
-	WITH "CTE" AS
+WITH "CTE" AS
 	(
 		SELECT
 			T1.CHAIN_ID
@@ -18,14 +17,28 @@
 			, T1.EVENT_START
 			, T1.EVENTP_START
 	)
-	, HIER as
+	, "HIER" AS
 	(
-		SELECT  *
+		SELECT
+			"HIERARCHY_RANK"
+			, "HIERARCHY_TREE_SIZE"
+			, "HIERARCHY_PARENT_RANK"
+			, "HIERARCHY_ROOT_RANK"
+			, "HIERARCHY_LEVEL"
+			, "HIERARCHY_IS_CYCLE"
+			, "HIERARCHY_IS_ORPHAN"
+			, "CHAIN_ID"
+			, "EVENT_START"
+			, "EVENTP_START"
+			, "EVENTP_GREEN"
+			, "TYPE"
+			, "VARIANTE"
+			, "NODE_ID"
+			, "PARENT_ID"
 		FROM
 			HIERARCHY ( SOURCE
 			(
-				SELECT
-					*
+				SELECT   *
 					, EVENTP_GREEN node_id
 					, EVENTP_START parent_id
 				FROM
@@ -33,22 +46,78 @@
 			)
 			START WHERE EVENTP_START = '' ORPHAN IGNORE )
 	)
-	
-SELECT * FROM
-	(	
-		SELECT
-			*
-		FROM
-			HIER
-		ORDER BY
-			CHAIN_ID
-			, hierarchy_rank
-			, hierarchy_tree_size
-	)
-WHERE
+	, "RSPC_HIERARCHY" AS
 	(
-		"HIERARCHY_ROOT_RANK"   = '1'
-		AND "HIERARCHY_IS_CYCLE" <> '1'
+		SELECT
+			CHAIN_ID
+			, EVENT_START
+			, EVENTP_START
+			, EVENTP_GREEN
+			, TYPE
+			, VARIANTE
+			, NODE_ID
+			, PARENT_ID
+			, hierarchy_rank
+		FROM
+			(
+				SELECT   *
+				FROM
+					HIER
+				ORDER BY
+					CHAIN_ID
+					, hierarchy_rank
+					, hierarchy_tree_size
+			)
+		WHERE
+			(
+				"HIERARCHY_ROOT_RANK"     = '1'
+				AND "HIERARCHY_IS_CYCLE" <> '1'
+			)
 	)
-
-;
+	, "PC_TGT_ORDER" AS
+	(
+		SELECT
+			"CHAIN_ID"
+			, DENSE_RANK() OVER ( ORDER BY CHAIN_ID ) CHAIN_NO
+			, "VARIANTE"
+			, ROW_NUMBER() OVER ()                                                 ROW_NUMBER
+			, T1."TGT"                                                             TARGET
+			, LAG(T1."TGT") OVER ( ORDER BY "CHAIN_ID" ASC , "HIERARCHY_RANK" ASC) LAG_TARGET
+			, HIERARCHY_RANK
+		FROM
+			"RSPC_HIERARCHY"
+			LEFT JOIN
+				(
+					SELECT   *
+					FROM
+						"SAPABAP1"."RSBKDTP"
+					WHERE
+						(
+							"OBJVERS" = 'A'
+						)
+				)
+				T1
+				ON
+					RSPC_HIERARCHY.VARIANTE = T1.DTP
+		WHERE
+			"TYPE" = 'DTP_LOAD'
+		ORDER BY
+			"CHAIN_ID" ASC --, "VARIANTE" ASC
+			, "HIERARCHY_RANK" ASC
+	)
+SELECT
+	CHAIN_ID
+	, CHAIN_NO
+	, VARIANTE
+	, ROW_NUMBER
+	, TARGET
+	, CASE
+		WHEN TARGET <> LAG_TARGET
+			THEN ROW_NUMBER + 1
+			ELSE ROW_NUMBER
+	END as TGT_NO
+FROM
+	PC_TGT_ORDER
+ORDER BY
+	CHAIN_NO
+	, TGT_NO
